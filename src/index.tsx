87f1cce4da5a981deffee3d1f73e85ee111510a1
Shay Cojocaru <shayc@outlook.com>
2020-04-07 02:12:53 +0300

    .

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,13 @@
-import { useSpeech, SpeechProvider } from './speech-context';
-export { useSpeech, SpeechProvider };
+export {
+  MIN_PITCH,
+  MIN_RATE,
+  MIN_VOLUME,
+  MAX_PITCH,
+  MAX_RATE,
+  MAX_VOLUME,
+  DEFAULT_PITCH,
+  DEFAULT_RATE,
+  DEFAULT_VOLUME,
+} from '@shayc/async-speech';
+
+export { useSpeech, SpeechProvider } from './speech-context';
--- src/speech-context.tsx
+++ src/speech-context.tsx
@@ -13,18 +13,6 @@ import {
   createAsyncSpeech,
 } from '@shayc/async-speech';
 
-export {
-  MIN_PITCH,
-  MIN_RATE,
-  MIN_VOLUME,
-  MAX_PITCH,
-  MAX_RATE,
-  MAX_VOLUME,
-  DEFAULT_PITCH,
-  DEFAULT_RATE,
-  DEFAULT_VOLUME,
-} from '@shayc/async-speech';
-
 interface SpeechContext {
   boundary: any;
   isPaused: boolean;
